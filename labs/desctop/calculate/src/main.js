467bd3d2ddaf6237b55cbc016e7b83e99b1afb7c
Gvidow <arzamachev.art-alex@yandex.ru>
2023-02-10 07:56:32 +0300

    added tauri-app

---
+++ labs/desctop/calculate/src/main.js
@@ -0,0 +1,17 @@
+const { invoke } = window.__TAURI__.tauri;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#result");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document
+    .querySelector("#btn_op_clear")
+    .addEventListener("click", () => greet());
+});
