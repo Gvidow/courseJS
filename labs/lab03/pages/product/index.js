40530a3f04589c379c9f67e4fd251525fbc7e064
Gvidow <arzamachev.art-alex@yandex.ru>
2023-03-16 16:08:21 +0300

    write lab 3

---
+++ labs/lab03/pages/product/index.js
@@ -0,0 +1,48 @@
+import { ProductComponent } from "../../components/product/index.js";
+import { MainPage } from "../main/index.js";
+import { BackButtonComponent } from "../../components/back-button/index.js";
+export class ProductPage {
+    constructor(parent, data) {
+        this.parent = parent;
+        this.data = data;
+    }
+    get getData() {
+        return this.data;
+    }
+    get pageRoot() {
+        return document.getElementById('product-page');
+    }
+
+    getHTML() {
+        return (
+            `
+                <div id="product-page"></div>
+            `
+        )
+    }
+    clickBack(node) {
+        let html = (`<div id="custom_info_alert" class="alert alert-info alert-dismissible" role="alert" style="width:370px; padding: 0.375rem 0.75rem; margin-left: 5px; position: relative; top: 8.5px; display: inline-flex"> 
+        <img src="components/icons/info.svg" width="25px" height="25px">
+        Этот кот не хочет чтобы вы уходили
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Закрыть" style="padding: 0.375rem 0.75rem; margin-left: 10px; position: relative; top: 1.7px;"></button></div>`)
+        this.pageRoot.insertAdjacentHTML('beforeend', html)
+        document
+            .getElementById("custom_info_alert")
+            .addEventListener("click", this.clickHome.bind(this));
+    }
+    clickHome() {
+        const mainPage = new MainPage(this.parent)
+        mainPage.render()
+    }
+    render() {
+        this.parent.innerHTML = ''
+        const html = this.getHTML()
+        this.parent.insertAdjacentHTML('beforeend', html)
+        const backButton = new BackButtonComponent(this.pageRoot)
+        backButton.render(this.clickBack.bind(this));
+        const data = this.getData
+        const product = new ProductComponent(this.pageRoot);
+        product.render(data)
+        
+    }
+}
