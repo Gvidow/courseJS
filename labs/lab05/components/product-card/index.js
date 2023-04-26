528364ce830734d8c2029ddbaf370e8307e90a83
Gvidow <arzamachev.art-alex@yandex.ru>
2023-04-26 14:38:19 +0300

    start lab5

---
+++ labs/lab05/components/product-card/index.js
@@ -0,0 +1,36 @@
+export class ProductCardComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    getHTML(data) {
+        return (
+            `
+                <div class="card" style="width: 300px;">
+                    <img class="card-img-top" src="${data.photo_400_orig}" alt="картинка">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.first_name}</h5>
+                        <div style="height: 120px"><p class="card-text">${data.last_name}</p></div>
+                        <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">
+                        
+                        Подробнее
+                        </button>
+                    </div>
+                </div>
+            `
+            // <p style="margin: 0px; height: auto; font-size: 0.5rem; float:right; border: 2px; margin-left: 4px;">${data.counter}</p>
+        )
+    }
+    
+    addListeners(data, listener) {
+        document
+            .getElementById(`click-card-${data.id}`)
+            .addEventListener("click", listener)
+    }
+    
+    render(data, listener) {
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+        this.addListeners(data, listener)
+    }
+}
